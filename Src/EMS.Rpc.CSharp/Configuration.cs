f25d747c8b1600acd60f37ae5d36233829bf2969
Angry Wasp <angrywasp@live.com>
2020-02-10 22:37:58 +1100

    Add project for an API interface for future GUI applications

---
+++ Src/EMS.Rpc.CSharp/Configuration.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Nerva.Rpc
+{
+    public class Log
+    {
+        public class Presets
+        {
+            //Log nothing
+            public static readonly Log None = new Log();
+
+            //Sane default. Logs errors and only logs rpc traffic when an error occurs
+            public static readonly Log Normal = new Log
+            {
+                LogRpcRequest = true,
+                LogRpcResponse = true,
+                LogRpcErrors = true,
+                LogNetworkErrors = true,
+                OnlyLogRpcOnError = true
+            };
+
+            //Log everything. Creates a lot of terminal spam
+            public static readonly Log Full = new Log
+            {
+                LogRpcRequest = true,
+                LogRpcResponse = true,
+                LogRpcErrors = true,
+                LogNetworkErrors = true,
+                OnlyLogRpcOnError = false
+            };
+        }
+
+        public List<int> SuppressRpcCodes { get; set; } = new List<int>();
+        public bool LogRpcRequest { get; set; } = false;
+        public bool LogRpcResponse { get; set; } = false;
+        public bool LogRpcErrors { get; set; } = false;
+        public bool LogNetworkErrors { get; set; } = false;
+
+        public bool OnlyLogRpcOnError { get; set; } = true;
+    }
+
+    public static class Config
+    {
+        public const uint DEFAULT_PORT = 3500;
+        public const string DEFAULT_HOST = "http://127.0.0.1";
+    }
+}
