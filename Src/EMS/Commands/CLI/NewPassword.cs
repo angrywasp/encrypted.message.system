80f56f110f32f4f3e5a35f776fd21da3e932d93a
Angry Wasp <angrywasp@live.com>
2020-02-25 22:26:02 +1100

    Add CLI command to change the key file password

---
+++ Src/EMS/Commands/CLI/NewPassword.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Text;
+using AngryWasp.Cryptography;
+using AngryWasp.Helpers;
+
+namespace EMS.Commands.CLI
+{
+    public class NewPassword
+    {
+        public static bool Handle(string command)
+        {
+            byte[] encryptedKeyData = File.ReadAllBytes(Config.User.KeyFile);
+            string a = PasswordPrompt.Get("Enter your key file password");
+            byte[] password = string.IsNullOrEmpty(a) ? Keccak.Hash128(HashKey16.Empty) : Keccak.Hash128(Encoding.ASCII.GetBytes(a));
+            byte[] decryptedKeyData = null;
+                
+            try
+            {
+                decryptedKeyData = Aes.Decrypt(encryptedKeyData, password);
+            }
+            catch
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Key file password was incorrect.");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+
+            string b = PasswordPrompt.Get("Enter a password for your key file");
+            string c = PasswordPrompt.Get("Confirm your password");
+
+            if (b != c)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Password do not match.");
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+
+            password = string.IsNullOrEmpty(b) ? Keccak.Hash128(HashKey16.Empty) : Keccak.Hash128(Encoding.ASCII.GetBytes(b));
+
+            encryptedKeyData = Aes.Encrypt(KeyRing.PrivateKey, password);
+
+            File.WriteAllBytes(Config.User.KeyFile, encryptedKeyData);
+            return true;
+        }
+    }
+}
